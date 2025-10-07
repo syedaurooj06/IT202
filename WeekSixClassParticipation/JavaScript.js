b375a6352a5a9b4ec3982c234d985fa5e13be83e
syedaurooj06 <154850590+syedaurooj06@users.noreply.github.com>
2025-10-07 13:34:04 -0400

    Add files via upload

---
+++ WeekSixClassParticipation/JavaScript.js
@@ -0,0 +1,7 @@
+function showWelcome() {
+    document.getElementById("message").style.display = "inline-block";
+}
+
+function hideWelcome() {
+    document.getElementById("message").style.display = "none";
+}
